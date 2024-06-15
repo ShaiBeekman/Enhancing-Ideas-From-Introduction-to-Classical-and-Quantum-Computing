caf3164d2bb4743441a84e485d2e102af7fb3587
Shai Beekman <shaibeekman@gmail.com>
2024-06-15 01:51:13 -0400

    potentially improved version of pg154 due to formatting being more easy to read

--- pg154.java
+++ pg154.java
@@ -1,20 +1,20 @@
 public class pg154 {
-    public String s1 = "CNOT|00> = |00>" +
-
-            ;
+    public String s1 = "CNOT|00> = |00>" +;
     //creating arbitrary generic situation denoted by "h"
     /*generic*/ boolean h
-    if (h) {
+    if(h)
+
+    {
         //creating arbitrary result of generic situation denoted by "w"
-        "CNOT|00> = |00>" = "CNOT|00> = |"+w+">"
+        "CNOT|00> = |00>" = "CNOT|00> = |" + w + ">"
     }//all this because left qubit is the control qubit, and the right qubit is the target qubit, this is all the information i have about this currently. My intuition is that the control qubit is intended to affect the target qubit, which is why I have coded this as such. I will find out more about whether my code is how this structure is intended to operate as I read further
 
     public observation1() {
         /* String s3 = "CNOT|10> = |11>" */ //and// /* String s4 = "CNOT|11> = |10>" */ //both exist
-            //better to code each thing as individual comments for easy compiler reading once comment tokenization is fully considered
+        //better to code each thing as individual comments for easy compiler reading once comment tokenization is fully considered
         /*notice s3 and s4 are inverses of each other*/
-            //this could be used for a time reversal operation, or action reversal operation
-                //meaning storage needs to be implemented, so that when multiple words can be used an author can include the multiplel possibly word choices in a word box, this could make reading more accurate and fulfilling. For example, I would like to say both "used for a" and also "used to impelement a" on l16 for 100% accuracy. So I have to make this possible with some kind of word-box to store both pieces of information. Of course, this is for any line or word that you would like to implement multiple meanings toward.
+        //this could be used for a time reversal operation, or action reversal operation
+        //meaning storage needs to be implemented, so that when multiple words can be used an author can include the multiplel possibly word choices in a word box, this could make reading more accurate and fulfilling. For example, I would like to say both "used for a" and also "used to impelement a" on l16 for 100% accuracy. So I have to make this possible with some kind of word-box to store both pieces of information. Of course, this is for any line or word that you would like to implement multiple meanings toward.
     }
 }
 
